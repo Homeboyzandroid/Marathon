c9693e15eab266c83163584f217a60f7822085a0
Homeboyz <homeboyz@Homeboyzs-iMac-4.local>
2016-01-12 11:09:08 +0300

    all code

--- app/src/main/java/com/beyondzero/loise/marathon/RegistrationInfo.java
+++ app/src/main/java/com/beyondzero/loise/marathon/RegistrationInfo.java
@@ -1,13 +1,26 @@
 package com.beyondzero.loise.marathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationInfo extends AppCompatActivity {
+
+    Button btnok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_info);
+
+        btnok = (Button)findViewById(R.id.btn_gotit);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+            }
+        });
     }
 }
