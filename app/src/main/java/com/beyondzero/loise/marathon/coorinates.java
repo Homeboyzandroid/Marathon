06f133ae95a2e2bf5e5907ae32faf7e0f41db00b
Faith Muli <faithmuli33@gmail.com>
2016-01-18 15:00:27 +0300

    10 kms route mapped

---
+++ app/src/main/java/com/beyondzero/loise/marathon/coorinates.java
@@ -0,0 +1,53 @@
+package com.beyondzero.loise.marathon;
+
+/**
+ * Created by faith on 1/18/16.
+ */
+public class coorinates {
+
+
+    /*-1.302810, 36.825408
+            -1.302614, 36.825328
+    //haile selasse
+            -1.292930, 36.820419
+            -1.292828, 36.820209
+
+    //cathedral
+            -1.292828, 36.820209
+            1.293177, 36.816586
+    //ngong road
+            -1.290259, 36.814210
+
+    //mbagathi
+            -1.297896, 36.803462
+            -1.298400, 36.803065
+    //t-mall
+            -1.312565, 36.815619
+            -1.312350, 36.816021
+
+
+  //  for 10km enter stadium through aerodrome road gatee A
+    -1.304820, 36.822780
+            -1.305099, 36.823134
+            -1.305002, 36.824137
+            -1.304846, 36.824378
+            -1.304782, 36.824979
+            -1.304128, 36.824233
+
+    //21KM AT madaraka roundabout
+            -1.304128, 36.824233
+            -1.306938, 36.819883
+            -1.305243, 36.826277
+            // joining mombasa road
+            -1.305313, 36.826513
+    //turning point libra hoouse
+            -1.330728, 36.866467
+            -1.330690, 36.866424
+    //nyayo round about return
+            -1.305193, 36.826366
+            -1.304941, 36.826151
+            -1.305156, 36.825727
+    //aerodrome road turn right
+            -1.306242, 36.820574
+    //takes the 10km route*/
+}
