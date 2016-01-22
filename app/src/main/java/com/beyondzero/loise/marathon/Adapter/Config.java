a48bf0608049f1d242ae913623becaacf1fe3849
loisenjoki <loisenjoki6@gmail.com>
2016-01-22 09:31:49 +0300

    fridays commit

---
+++ app/src/main/java/com/beyondzero/loise/marathon/Adapter/Config.java
@@ -0,0 +1,12 @@
+package com.beyondzero.loise.marathon.Adapter;
+
+/**
+ * Created by homeboyz on 1/21/16.
+ */
+public class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyBILUvCEP_6s9EhVgGLGsOMExAbKMJrjUk";
+}
---
+++ app/src/main/java/com/beyondzero/loise/marathon/RouteList.java
@@ -0,0 +1,48 @@
+package com.beyondzero.loise.marathon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class RouteList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_list);
+
+       /* getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.flhmnewlogo);*/
+
+        String[] myDataset = {"21 Kms Route", "10 Kms Route", "5 kms Route", "2 Kms Route",};
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myDataset);
+
+        ListView myList = (ListView) findViewById(R.id.listView1);
+        myList.setAdapter(myAdapter);
+
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        startActivity(new Intent(getApplicationContext(), RoutesActivity.class));
+                        break;
+                    case 1:
+                        startActivity(new Intent(getApplicationContext(), TenKms.class));
+                        break;
+                    case 2:
+                        startActivity(new Intent(getApplicationContext(), Fivekm.class));
+                        break;
+                    case 3:
+                        startActivity(new Intent(getApplicationContext(), RoutesActivity.class));
+                        break;
+                }
+            }
+        });
+    }
+}
