49ae3c6430130b05548dc713ceb92f7c69e68e2e
Faith Muli <faithmuli33@gmail.com>
2016-01-25 11:27:30 +0300

    2kms done 5kms lots of errors

--- app/src/main/java/com/beyondzero/loise/marathon/Fivekm.java
+++ app/src/main/java/com/beyondzero/loise/marathon/Fivekm.java
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 public class Fivekm extends FragmentActivity implements OnMapReadyCallback {
@@ -121,7 +122,84 @@ public class Fivekm extends FragmentActivity implements OnMapReadyCallback {
                         new LatLng(-1.292884, 36.820278),
                         new LatLng(-1.292777, 36.820203),
                         new LatLng(-1.292605, 36.820278),
-                        new LatLng(-1.292530, 36.820224)
+                        new LatLng(-1.292530, 36.820224),
+                        //stl on uhuru highway
+                        new LatLng(-1.292423, 36.820149),
+                        new LatLng(-1.291179, 36.819537),
+                        new LatLng(-1.289763, 36.818840),
+                        new LatLng(-1.288230, 36.818068),
+                        new LatLng(-1.286889, 36.817414),
+                        new LatLng(-1.286814, 36.817350),
+                        new LatLng(-1.286782, 36.817221),
+                        new LatLng(-1.286718, 36.817178),
+                        new LatLng(-1.286718, 36.817071),
+                        new LatLng(-1.286793, 36.816953),
+                        new LatLng(-1.286847, 36.816803),
+                        new LatLng(-1.286944, 36.816610),
+                        new LatLng(-1.287255, 36.815998),
+                        new LatLng(-1.287834, 36.814818),
+                        new LatLng(-1.287888, 36.814721),
+                        //PROCESIONAL WAY TURN LEFT
+                        new LatLng(-1.287985, 36.814667),
+                        new LatLng(-1.288103, 36.814721),
+                        new LatLng(-1.288725, 36.815054),
+                        //Procesional way
+                        new LatLng(-1.289283, 36.815365),
+                        new LatLng(-1.289819, 36.815655),
+                        new LatLng(-1.290977, 36.816224),
+                        new LatLng(-1.291331, 36.816396),
+                        new LatLng(-1.291513, 36.816439),
+                        new LatLng(-1.291652, 36.816471),
+                        new LatLng(-1.292639, 36.816546),
+                        new LatLng(-1.293036, 36.816589),
+                        new LatLng(-1.293175, 36.816600),
+                        new LatLng(-1.293263, 36.816617),
+                        new LatLng(-1.293373, 36.816647),
+                        new LatLng(-1.293373, 36.816647),
+                        new LatLng(-1.293419, 36.816666),
+                        //hailesellase turn left
+                        new LatLng(-1.293454, 36.816696),
+                        new LatLng(-1.293414, 36.816960),
+                        new LatLng(-1.293267, 36.817741),
+                        new LatLng(-1.292959, 36.819227),
+                        new LatLng(-1.292865, 36.819788),
+                        new LatLng(-1.292844, 36.819863),
+                        new LatLng(-1.292847, 36.820166),
+                        new LatLng(-1.292866, 36.820244),
+                        new LatLng(-1.292890, 36.820319),
+                        //UHURU HIGHWAY BACK T STARTING POINT
+                        new LatLng(-1.292914, 36.820383),
+                        new LatLng(-1.293045, 36.820455),
+                        new LatLng(-1.293558, 36.820764),
+                        new LatLng(-1.294167, 36.821134),
+                        new LatLng(-1.294167, 36.821134),
+                        new LatLng(-1.295028, 36.821688),
+                        new LatLng(-1.295125, 36.821739),
+                        new LatLng(-1.295415, 36.821897),
+                        new LatLng(-1.296284, 36.822407),
+                        new LatLng(-1.297046, 36.822863),
+                        new LatLng(-1.298038, 36.823440),
+                        new LatLng(-1.298384, 36.823641),
+                        new LatLng(-1.298540, 36.823721),
+                        new LatLng(-1.298934, 36.823839),
+                        new LatLng(-1.299285, 36.823922),
+                        new LatLng(-1.299518, 36.823954),
+                        new LatLng(-1.299698, 36.823978),
+                        //BUNYALA ROUND ABOUT
+                        new LatLng(-1.299730, 36.823973),
+                        new LatLng(-1.299794, 36.823943),
+                        new LatLng(-1.299842, 36.823932),
+                        new LatLng(-1.299896, 36.823935),
+                        new LatLng(-1.299963, 36.823970),
+                        new LatLng(-1.300006, 36.824018),
+                        new LatLng(-1.300041, 36.824093),
+                        new LatLng(-1.300084, 36.824141),
+                        new LatLng(-1.300540, 36.824334),
+                        new LatLng(-1.301044, 36.824562),
+                        new LatLng(-1.301757, 36.824903),
+                        new LatLng(-1.301958, 36.824994),
+                        new LatLng(-1.302000, 36.825024),
+                        new LatLng(-1.302483, 36.825244)
                 );
         Polyline shortrace=mMap.addPolyline(fivekms);
                            }
